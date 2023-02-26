d6707fd40f6289c6c841dfc45240b5d13346f49e
PhStefen <raph.stefen@gmail.com>
2023-02-26 11:59:34 -0300

    Project Setup

---
+++ test/jest.config.js
@@ -0,0 +1,10 @@
+const { resolve } = require("path");
+const rootDir = resolve(__dirname, "..");
+const rootConfig = require(`${rootDir}/jest.config.js`)
+module.exports = {
+    ...rootConfig,
+    rootDir,
+    displayName: "end2end-tests",
+    setupFilesAfterEnv: ["<rootDir>/test/jest-setup.ts"],
+    testMatch: ["<rootDir>/test/**/*.test.ts"]
+} 
